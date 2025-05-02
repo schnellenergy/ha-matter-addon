4b831786222a6d72390e32082fed1f20f9358856
veeramanikandan r <veeramanikandan.r@schnellenergy.com>
2025-05-02 16:40:22 +0530

    Create minimal Matter Controller add-on

--- matter-controller/run.sh
+++ matter-controller/run.sh
@@ -6,6 +6,12 @@ LOG_LEVEL=$(bashio::config 'log_level')
 # Create data directory
 mkdir -p /data/matter_server
 
+# Print some debug info
+bashio::log.info "Python version:"
+python3 --version
+bashio::log.info "Installed packages:"
+pip3 list
+
 # Start the Matter Server
 bashio::log.info "Starting Matter Server..."
 python3 -m matter_server.server \
